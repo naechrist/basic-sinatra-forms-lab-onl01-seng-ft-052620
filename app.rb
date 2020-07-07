ea9138911a175b20125844e90483db501b913181
Jenae Christensen <729jenae.christensen@gmail.com>
2020-07-07 04:38:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,13 +1,13 @@
-require 'sinatra/base'
+# require 'sinatra/base'
 
-class App < Sinatra::Base
+# class App < Sinatra::Base
 
-  get '/newteam' do 
-    erb :newteam
-  end 
-  post '/team' do 
-    puts params
-    erb :team 
-  end 
+#   get '/newteam' do 
+#     erb :newteam
+#   end 
+#   post '/team' do 
+#     puts params
+#     erb :team 
+#   end 
 
-end
+# end
